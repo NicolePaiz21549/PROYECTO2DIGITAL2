e1c9ae02ee32b6e4967123011329ee34a0194480
NicolePaiz21549 <84263290+NicolePaiz21549@users.noreply.github.com>
2023-10-30 11:10:45 -0600

    Update ili9341UARTBUZZ.ino

--- TIVA C FOR PROYECTO 2/ili9341UARTBUZZ/ili9341UARTBUZZ.ino
+++ TIVA C FOR PROYECTO 2/ili9341UARTBUZZ/ili9341UARTBUZZ.ino
@@ -32,16 +32,17 @@
 #define LCD_WR PD_3
 #define LCD_RD PE_1
 int DPINS[] = {PB_0, PB_1, PB_2, PB_3, PB_4, PB_5, PB_6, PB_7};*/  
-#define BSENSE PF_0 //Botón para preguntar el valor del sensor al ESP32
+#define BSENSE PF_4 //Botón para preguntar el valor del sensor al ESP32
 #define RXp2 PD6
 #define TXp2 PD7
 //***************************************************************************************************************************************
 
 //Variables globales 
-int receivedvaluesensor=0; //Variable determinada para recibir el valor del sensor LM35
+int receivedvaluesensor; //Variable determinada para recibir el valor del sensor LM35
+int clave=0; //Aviso al ESP32 de presión del botón BSENSE
 unsigned long lastDebounceTime=0;
 unsigned long debounceDelay=50;
-bool requested=false; //Bandera para identificar si se ha realizado un pedido de valor aka request
+//bool requested=false; //Bandera para identificar si se ha realizado un pedido de valor aka request
 //***********************************************************************************
 
 //Prototipos de funciones
@@ -61,19 +62,14 @@ void loop(){
   //Serial.print("∖n");
   if(digitalRead(BSENSE) == LOW && (millis()-lastDebounceTime)>debounceDelay){
     lastDebounceTime = millis();
-    Serial2.print('R'); //Pedir al ESP32 el valor del sensor
+    clave=1; 
+    Serial2.print(clave); //Pedir al ESP32 el valor del sensor
     Serial2.print("∖n");
-    
-    //if(requested){//Verificar si no se ha realizado un pedido anteriormente
-    //Serial2.println('R'); 
-    //requested=true; //Bandera en positivo para enviar múltiples pédidos
-    //}
-    /*if (Serial2.available()>0){ //Utilizar Serial2 para comunicarse con el ESP32
-      String response=ReadStringUrial2.rntil('\n');
-      if(response=="LM35"){
+    }
+    if (Serial2.available()>0){ //Utilizar Serial2 para comunicarse con el ESP32
         receivedvaluesensor=Serial2.parseInt();
-        Serial.print(response);
-        Serial.println(receivedvaluesensor);*/
+        Serial.print("LM35:");
+        Serial.println(receivedvaluesensor);
         }
         }
 //***************************************************************************************************************************************
