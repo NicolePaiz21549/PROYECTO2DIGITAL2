dbf3520d7da970441cde69753c31ab5a5eff6a41
NicolePaiz21549 <84263290+NicolePaiz21549@users.noreply.github.com>
2023-10-29 19:09:49 -0600

    Update main.cpp

--- SENSOR - ESP32/src/main.cpp
+++ SENSOR - ESP32/src/main.cpp
@@ -12,16 +12,14 @@
 
 //Definición de pines
 #define LM35_GPIO_PIN 35 //Pin para el sensor LM35
-//#define BUTTON_PIN 33 //Botón para grabar la lectura actual de temperatura via Tiva C
+#define RXp2 16
+#define TXp2 17
 //***********************************************************************************
 
 //Variables globales 
 int LM35_Input = 0;
 float TempC = 0.0;
 float Voltage = 0.0;
-//bool buttonPressed = false;
-//unsigned long previousMillis = 0;  
-//const long interval = 500;
 //***********************************************************************************
 
 //Prototipos de funciones
@@ -35,38 +33,25 @@ uint32_t readADC_Cal(int ADC_Raw) {
 
 //Configuración
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(115200); //Comunicación con el monitor serial/PC
+  Serial2.begin(115200, SERIAL_8N1, RXp2, TXp2); //Comunicación UART 2 con la Tiva C 
   pinMode(LM35_GPIO_PIN, INPUT); //Inicialización del pin LM35
 //pinMode(BUTTON_PIN, INPUT_PULLUP); //Botón con resistencia de pull-up interna
   }
 
 //Loop principal
 void loop() {
-  
-  unsigned long currentTime = millis();
-  //int buttonState = digitalRead(BUTTON_PIN);
+// Display de temperatura en monitor serial 
+int tempInt = int(TempC);
+int tempDecimal = int((TempC - tempInt) * 10); //Cálculo del decimal del valor de temperatura, lo multiplica por 10 para convertirlo en una fracción de 10 grados y almacena el resultado como un número entero en la variable tempDecimal.
+int tempDecimalTens = tempInt / 10;
+int tempDecimalUnits = tempInt % 10;
 
-      // Display de temperatura en monitor serial 
-      int tempInt = int(TempC);
-      int tempDecimal = int((TempC - tempInt) * 10); //Cálculo del decimal del valor de temperatura, lo multiplica por 10 para convertirlo en una fracción de 10 grados y almacena el resultado como un número entero en la variable tempDecimal.
-      int tempDecimalTens = tempInt / 10;
-      int tempDecimalUnits = tempInt % 10;
-
-  //if ((digitalRead(BUTTON_PIN) == LOW)){
-    //if (!buttonPressed) {
-      //io.run();
-      //buttonPressed = true;
-
-      // Lectura del valor LM35_ADC
-      TempC = ((analogRead(LM35_GPIO_PIN) + 70) * (5000.0/ 4096.0)); // Sumatoria de un valor OFFSET (factor de correción) para el valor análogo original multiplicado por el voltaje de 5V por pin Vin para la fórmula dividido la resolución ADC de 12bits
-      TempC = TempC / 10.0; // División dentro de 10 representando los 10mV del LM35 ya que cada cambio de 10mV representa un cambio de 1℃
-      double Temperature = TempC; 
-      //if (currentTime - previousMillis >= interval) {
-        //previousMillis = currentTime;
-        //sensortemperatura->save(Temperature);
-      //}
-
-      Serial.print(TempC); //Impresión de backup
-      Serial.println();
+//Lectura del valor LM35_ADC
+TempC = ((analogRead(LM35_GPIO_PIN) + 70) * (5000.0/ 4096.0)); // Sumatoria de un valor OFFSET (factor de correción) para el valor análogo original multiplicado por el voltaje de 5V por pin Vin para la fórmula dividido la resolución ADC de 12bits
+TempC = TempC / 10.0; // División dentro de 10 representando los 10mV del LM35 ya que cada cambio de 10mV representa un cambio de 1℃
+double Temperature = TempC;
+Serial.print(TempC); //Impresión de backup
+Serial.println();
 }
 //***********************************************************************************
