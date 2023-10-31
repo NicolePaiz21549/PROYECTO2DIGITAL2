815dcb191a67fda9c3e9e91244b9922aae4ff75b
NicolePaiz21549 <84263290+NicolePaiz21549@users.noreply.github.com>
2023-10-31 15:36:30 -0600

    Update ili9341UARTBUZZ.ino

--- TIVA C FOR PROYECTO 2/ili9341UARTBUZZ/ili9341UARTBUZZ.ino
+++ TIVA C FOR PROYECTO 2/ili9341UARTBUZZ/ili9341UARTBUZZ.ino
@@ -38,7 +38,7 @@ int DPINS[] = {PB_0, PB_1, PB_2, PB_3, PB_4, PB_5, PB_6, PB_7};*/
 #define BSD PF_0 //Botón para guardar el valor de la medición  del sensor en el SD - SW2
 #define RXp2 PD6
 #define TXp2 PD7
-#define CS_PIN PA_3 //aka pin 12 para el chip select 
+#define CS_PIN PB_3 //aka pin 12 para el chip select 
 //***************************************************************************************************************************************
 
 //Variables globales 
@@ -46,53 +46,56 @@ float receivedvaluesensor; //Variable determinada para recibir el valor del sens
 int clave=0; //Aviso al ESP32 de presión del botón BSENSE
 unsigned long lastDebounceTime=0;
 unsigned long debounceDelay=50;
-File root; //Indicador del directorio de la raíz de la SD
-File dataFile; 
+//File root; //Indicador del directorio de la raíz de la SD
+//File dataFile; 
 //bool requested=false; //Bandera para identificar si se ha realizado un pedido de valor aka request
 //***********************************************************************************
 
 //Prototipos de funciones
-void printDirectory(File dir, int numTabs) { //Función encargada de la enumeración del contenido del directorio del SD
-  while(true){
-    File entry=dir.openNextFile();
-    if(!entry){
-      break;
-      }
-      for(uint8_t i=0; i<numTabs; i++){
-        Serial.print('\t');
-        }
-        Serial.print(entry.name());
-        if(entry.isDirectory()){
-          Serial.println("/");
-          printDirectory(entry, numTabs+1);
-          }else{
-            Serial.print("\t\t");
-            Serial.println(entry.size(), DEC);
-            }
-            entry.close();
-            }
-}
+//void printDirectory(File dir, int numTabs) { //Función encargada de la enumeración del contenido del directorio del SD
+  //while(true){
+    //File entry=dir.openNextFile();
+    //if(!entry){
+      //break;
+      //}
+      //for(uint8_t i=0; i<numTabs; i++){
+        //Serial.print('\t');
+        //}
+        //Serial.print(entry.name());
+        //if(entry.isDirectory()){
+          //Serial.println("/");
+          //printDirectory(entry, numTabs+1);
+          //}else{
+            //Serial.print("\t\t");
+            //Serial.println(entry.size(), DEC);
+            //}
+            //entry.close();
+            //}
+//}
 //***************************************************************************************************************************************
 
 //Configuración
 void setup(){
   Serial.begin(115200); //Inicialización de la comunicación serial con el monitor serial
-  SPI.setModule(0); //Implementación del módulo SPIO por la pantalla ili9341
   Serial2.begin(115200); //Comunicación UART con ESP32
+  SPI.setModule(0); //Implementación del módulo SPIO por la pantalla ili9341
   
-  pinMode(BSENSE, INPUT_PULLUP); //Configuración de SW1
-  pinMode(BSD, INPUT_PULLUP); //Configuración de SW1
-
   Serial.print("Inicializando la tarjeta SD...");
   if(!SD.begin(CS_PIN)){
     Serial.println("¡Falló la inicialización de la tarjeta!D:");
-    while(true); 
+    //while(true);
+    return; 
   }
   Serial.println("Inicialización realizada:D");
-  Serial.println("Archivos en la tarjeta SD:");
-  root=SD.open("/");
-  printDirectory(root,0);
-  Serial.println("");
+  //Serial.println("Archivos en la tarjeta SD:");
+  //root=SD.open("/");
+  //printDirectory(root,0);
+  //Serial.println("");
+  
+  //Configuración de los SW  
+  pinMode(BSENSE, INPUT_PULLUP); //Configuración de SW1
+  pinMode(BSD, INPUT_PULLUP); //Configuración de SW1
+  pinMode(RED_LED, OUTPUT);
   }
 //***************************************************************************************************************************************
 
@@ -101,6 +104,7 @@ void loop(){
   //Verificación de BSENSE para realizar la medición del sensor
   //Serial.print("∖n");
   if(digitalRead(BSENSE) == LOW && (millis()-lastDebounceTime)>debounceDelay){
+    digitalWrite(RED_LED, HIGH);
     lastDebounceTime = millis();
     clave=1; 
     Serial2.print(clave); //Pedir al ESP32 el valor del sensor
@@ -117,22 +121,19 @@ void loop(){
    if(digitalRead(BSD)==LOW && (millis()-lastDebounceTime)>debounceDelay){
     lastDebounceTime=millis();
     //if(dataFile){
-      dataFile=SD.open("sensor_data.txt", FILE_WRITE); //Apertura del archivo en modo escritura
-      if(!dataFile){
-        dataFile.print("LM35:");
+      File dataFile=SD.open("datalog.txt", FILE_WRITE); //Apertura del archivo en modo escritura
+      if(dataFile){
         dataFile.println(receivedvaluesensor);
         dataFile.close(); //Se cierra el archivo
-        Serial.print("Se ha guardado exitosamente la medición de la temperatura en la tarjeta SD.");
-        //Serial.println("");
-        //Serial.println("");
-        root=SD.open("/");
-        printDirectory(root, 0);
-        //Serial.println("");
+        Serial.println("Se ha guardado exitosamente la medición de la temperatura en la tarjeta SD.");
+        Serial.println(receivedvaluesensor);
         //Agregar melodía indicativa que se almacenó un dato en la memoria SD
-        } else {
-          Serial.println("Error al abrir y crear el archivo en la tarjeta SD.");
-            }
-            //}
         }
+        //Si no fue posible abrir el archivo→ERROR
+        else{
+          Serial.println("Error al abrir el archivo datalog.txt y no se ha guardado la medida de temperatura");
+        }
+        }
+        delay(20);
 }
 //***************************************************************************************************************************************
